eb8031e1d994b28880b13d78adbf4f111b787ea1
tanishkatriescoding <tanishka04suryawanshi@gmail.com>
2026-02-05 18:26:02 +0530

    Fix db for railway deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,11 @@
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-     host: "localhost",
-     port: 5432,
-     user: "postgres",
-     password: process.env.DB_PASSWORD,
-     database: "task_management",
+     connectionString: process.env.DATABASE_URL,
+     ssl: process.env.NODE_ENV === "production"
+     ? { rejectUnauthorized: false }
+     : false,
 });
 
 export default pool;
-await pool.query("SET search_path TO public");
